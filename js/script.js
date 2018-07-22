d578876dba288cb4273b892d9584f4ef8545edd2
Thomas Ngo <thoas015@gmail.com>
2018-07-22 16:13:30 -0400

    Project description toggle complete. Starting image carousel.

--- js/script.js
+++ js/script.js
@@ -11,7 +11,26 @@ $(document).ready(function () {
         } else {
             $('#main-nav').slideUp(500);
         }
-    })
+    });
 
-})
+    /* Description Display Toggle */
+
+    let projectDescriptionToggleButtons = document.getElementsByClassName('projectDescriptionToggle');
+
+   
+    // TODO: figure out how to make it so I'm not limited to 10 projects because of the if/else JQuery ID Selectors
+    for (let i = 0; i < projectDescriptionToggleButtons.length; i++){
+        projectDescriptionToggleButtons[i].addEventListener('click', function () {
+            if (document.getElementsByClassName('projectDescriptionContainers')[i].style.display === "none" || document.getElementsByClassName('projectDescriptionContainers')[i].style.display === "" ) {
+                $('#projDescCon' + i).slideDown(500);
+            } else {
+                $('#projDescCon' + i).slideUp(500);
+            }
+        })
+    }
+
+  
+    
+
+});
 
